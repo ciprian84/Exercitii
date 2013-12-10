3a6a32b6b05b3d3606c5f60c01088e2221e9e2dc
Ciprian <buzdugan.ciprian@gmail.com>
2013-12-11 00:35:28 +0200

    Calcul 12345679 x 9; 12345679 x 18; 12345679 x 27; 12345679 x 36; 12345679 x 45; 12345679 x 54; 12345679 x 63; 12345679 x 72; 12345679 x 81.

---
+++ Exercitiul 8/Calcule.dpr
@@ -0,0 +1,19 @@
+program Calcule;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+begin
+  Writeln(output, FloatToStr(12345679*9));
+  Writeln(output, FloatToStr(12345679*18));
+  Writeln(output, FloatToStr(12345679*27));
+  Writeln(output, FloatToStr(12345679*36));
+  Writeln(output, FloatToStr(12345679*45));
+  Writeln(output, FloatToStr(12345679*54));
+  Writeln(output, FloatToStr(12345679*63));
+  Writeln(output, FloatToStr(12345679*72));
+  Writeln(output, FloatToStr(12345679*81));
+  Readln(Input);
+end.
+ 
