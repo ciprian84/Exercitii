e3fcdb0ec9fd6e8a2347c5f8909579ce4da0b944
Ciprian <buzdugan.ciprian@gmail.com>
2013-12-11 20:20:08 +0200

    Exercitiul 11 - afisare model: ***** ***** ***** ***** *****

---
+++ Exercitiul11/AfisareModel.dpr
@@ -0,0 +1,22 @@
+program AfisareModel;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+var
+  s: string;
+begin
+  s := '*****';
+  Writeln(Output, s);
+  Writeln('');
+  Writeln(Output, s);
+  Writeln('');
+  Writeln(Output, s);
+  Writeln('');
+  Writeln(Output, s);
+  Writeln('');
+  Writeln(Output, s);
+  Readln(Input);
+end.
+ 
