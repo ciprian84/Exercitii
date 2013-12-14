dc5ac6746235f4b27d19d718db6df8b46e0f4199
Ciprian <buzdugan.ciprian@gmail.com>
2013-12-14 18:53:45 +0200

    info persoana

---
+++ Exercitiul1.dpr
@@ -0,0 +1,21 @@
+program Exercitiul1;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+var
+ vNume, vOras: string;
+ vVarsta: Integer;
+begin
+ Writeln(Output, 'Introducere date persoana: ');
+ Writeln(Output, 'Nume: ');
+ Readln(Input, vNume);
+ Writeln(Output, 'Varsta: ');
+ Readln(Input, vVarsta);
+ Writeln(Output, 'Orasul: ');
+ Readln(Input, vOras);
+ Writeln('Date persoana ' + 'Nume: ' + vNume + ', varsta ' + IntToStr(vVarsta) + ', orasul ' + vOras);
+ Readln(Input);
+end.
